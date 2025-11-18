25bb044207a82685acad4bec8e5825e4e290eb24
kevilevi <kevin.kevlev822@gmail.com>
2025-11-18 20:37:43 +0530

    Update un.py

--- asset/un.py
+++ asset/un.py
@@ -1,13 +1,14 @@
 import pyautogui
 import subprocess
 import time
-path = r"C:\Users\murug\Downloads\winnnnnnnnnnnnn\winmonitor-common.exe"
+path = r"\\192.168.13.248\k\USHR_Groups\winmonitor-common.exe"
 subprocess.Popen(path)
 time.sleep(7)
 pyautogui.press("tab")                
 time.sleep(0.5)
-pyautogui.typewrite("12849", 0.05)    
+pyautogui.typewrite("15416", 0.05)    
 time.sleep(0.5)
 pyautogui.press("tab")                
 time.sleep(0.5)
 pyautogui.press("space")              
+
